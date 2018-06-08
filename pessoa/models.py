93c9f44b90c020790c9a0aaf12219cd5dd6c9047
Jessica Temporal <jessicatemporal@gmail.com>
2018-06-08 10:42:42 -0300

    Pet

--- pessoa/models.py
+++ pessoa/models.py
@@ -2,10 +2,18 @@ from schematics.models import Model
 from schematics.types import StringType, ListType, ModelType, IntType
 
 
+class PetType(Model):
+    nome = StringType(required=True, deserialize_from="name",
+                      serialized_name="name")
+    cor = StringType(required=True, deserialize_from="fur",
+                      serialized_name="fur")
+
+
 class Pessoa(Model):
-    nome = StringType(required=True, serialize_when_none=False,
-                      deserialize_from="name", serialized_name="name")
+    nome = StringType(required=True, deserialize_from="name",
+                      serialized_name="name")
     idade = IntType(serialize_when_none=False, deserialize_from="age",
                     serialized_name="age")
     linguagens = ListType(StringType, serialize_when_none=False,
                           deserialize_from="lang", serialized_name="blabla")
+    pets = ListType(ModelType(PetType))
